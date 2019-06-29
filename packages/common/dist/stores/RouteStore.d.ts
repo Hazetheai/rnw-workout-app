8ba8b3adf03375b2b5ca91e7451894163f93285f
Jake <jacobstefan92@gmail.com>
2019-06-28 23:51:46 -0400

    removed dist from tracking

--- packages/common/dist/stores/RouteStore.d.ts
+++
@@ -1,6 +0,0 @@
-import { WorkoutStore } from "./WorkoutStore";
-export declare class RootStore {
-    routerStore: any;
-    workoutStore: WorkoutStore;
-}
-export declare const RootStoreContext: import("istanbul-lib-report").Context;
--- packages/common/dist/stores/RoutersStore.d.ts
+++
@@ -1,8 +0,0 @@
-import { RootStore } from "./RouteStore";
-declare type Routes = "WorkoutHistory" | "CurrentWorkout";
-export declare class WorkoutStoreRouterStore {
-    rootStore: RootStore;
-    constructor(rootStore: RootStore);
-    screen: Routes;
-}
-export {};
