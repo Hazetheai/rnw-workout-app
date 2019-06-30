742ee15f9eb9b4adf71cce3d79db783a12bf74b7
Jake <jacobstefan92@gmail.com>
2019-06-29 21:26:13 -0400

    added touch action to view curr wrkout

--- packages/common/src/ui/HistoryCard.tsx
+++ packages/common/src/ui/HistoryCard.tsx
@@ -6,25 +6,30 @@ import { Card } from "./Card";
 interface Props {
   header: string;
   currentExercises: CurrentExercise[];
+  onPress: () => void;
 }
 
 const exerciseShortName = {
   Squat: "SQ",
-  DeadLift: "DL",
+  Deadlift: "DL",
   "Bench Press": "BP",
   "Overhead Press": "OHP",
   "Barbell Row": "ROW"
 };
 
-export const HistoryCard: React.FC<Props> = (header, currentExercises) => {
+export const HistoryCard: React.FC<Props> = ({
+  header,
+  currentExercises,
+  onPress
+}) => {
   return (
-    <Card>
+    <Card onPress={onPress}>
       <Text>{header}</Text>
-      {currentExercises.map((curEx: any) => {
+      {currentExercises.map(curEx => {
         return (
           <Text key={curEx.exercise}>{`${
             exerciseShortName[curEx.exercise as keyof typeof exerciseShortName]
-          }${curEx.numSets}x${curEx.reps} ${curEx.weight} `}</Text>
+          } - ${curEx.numSets} x ${curEx.reps} ${curEx.weight}lbs `}</Text>
         );
       })}
     </Card>
